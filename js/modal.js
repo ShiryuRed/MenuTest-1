74f3ad28de770f7af572563b699f57109cf2ae99
YaTo <62433632+ShiryuRed@users.noreply.github.com>
2024-01-27 19:45:16 -0600

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -2,16 +2,24 @@ const backgroundModal = document.querySelector('.modal-container');
 const modalImg = document.querySelector('.modal-img');
 
 backgroundModal.addEventListener('click', () => {
-  backgroundModal.setAttribute('hidden', 'true');
-  alert('ola')
+  imgModalContainer.classList.remove('img-modal-container-on')
+  setTimeout(function(){
+   backgroundModal.classList.add('close-modal-container');
+  },600);
+  
 })
 
 
 const modalImages = document.querySelectorAll('.img-menu-list');
-
+const imgModalContainer = document.querySelector('.img-modal-container');
 modalImages.forEach(images => {
   images.addEventListener('click', () => {
-    alert('a')
+    let src = images.getAttribute('src');
+    modalImg.setAttribute('src', src);
+    backgroundModal.classList.remove("close-modal-container");
+    setTimeout(function(){
+      imgModalContainer.classList.add('img-modal-container-on')
+    },100);
   });
 });
 
